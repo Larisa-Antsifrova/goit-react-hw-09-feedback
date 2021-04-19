ddd11ce914eefc2fc039ea91c9f09c3ae41eea4e
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-19 21:44:09 +0300

    Refactored App with hooks.

--- src/App.js
+++ src/App.js
@@ -57,38 +57,4 @@ const App = () => {
   );
 };
 
-// class App extends Component {
-//
-
-//   render() {
-//     const total = this.countTotalFeedback();
-//     const positivePercentage = this.countPositiveFeedbackPercentage();
-
-//     return (
-//       <>
-//         <PageTitle title="Espresso Cafe Stats" />
-
-//         <Section title="Please leave your feedback">
-//           <FeedbackOptions
-//             feedback={this.state}
-//             onLeaveFeedback={this.countFeedback}
-//           />
-//         </Section>
-
-//         <Section title="Statistics">
-//           {total ? (
-//             <Statistics
-//               feedback={this.state}
-//               total={total}
-//               positivePercentage={positivePercentage}
-//             />
-//           ) : (
-//             <Notification message="No feedback given" />
-//           )}
-//         </Section>
-//       </>
-//     );
-//   }
-// }
-
 export default App;
